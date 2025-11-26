2d7a029190365d24f3118436e3b4c0f3286fa869
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2025-11-26 12:49:05 -0600

    Implementación de clase service.

--- src/main/java/com/company/books/backend/service/CategoriaServiceImpl.java
+++ src/main/java/com/company/books/backend/service/CategoriaServiceImpl.java
@@ -1,15 +1,47 @@
 package com.company.books.backend.service;
 
-import org.springframework.stereotype.Service;
+import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.company.books.backend.model.Categoria;
+import com.company.books.backend.model.dao.ICategoriaDao;
 import com.company.books.backend.response.CategoriaResponseRest;
 
 @Service
 public class CategoriaServiceImpl implements ICategoriaService {
+	
+	private static final Logger log = LoggerFactory.getLogger(CategoriaServiceImpl.class);
+	
+	@Autowired
+	private ICategoriaDao categoriaDao;
 
 	@Override
+	@Transactional(readOnly = true)
 	public CategoriaResponseRest buscarCategorias() {
-		return null;
+		
+		log.info("Inicio de metodo buscarCategorias()");
+		
+		CategoriaResponseRest response = new CategoriaResponseRest();
+		
+		try {
+			
+			List<Categoria> categoria =  (List<Categoria>) categoriaDao.findAll();
+			
+			response.getCategoriaResponse().setCategoria(categoria);
+			response.setMetadata("Respuesta OK","200","Respuesta exitosa");
+			
+		} catch (Exception e) {
+			response.setMetadata("Respuesta NOT OK","-1","Respuesta incorrecta");
+			log.error("Error al consultar categorias: ", e.getMessage());
+			e.getStackTrace();
+		}
+		
+		return response;
 	}
 
 	

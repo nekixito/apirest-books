32a6087173772d7375e22bfa97edb27f89d7269e
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2025-11-26 12:32:18 -0600

    Creación de clases de servicio de Categoria.

---
+++ src/main/java/com/company/books/backend/service/CategoriaServiceImpl.java
@@ -0,0 +1,16 @@
+package com.company.books.backend.service;
+
+import org.springframework.stereotype.Service;
+
+import com.company.books.backend.response.CategoriaResponseRest;
+
+@Service
+public class CategoriaServiceImpl implements ICategoriaService {
+
+	@Override
+	public CategoriaResponseRest buscarCategorias() {
+		return null;
+	}
+
+	
+}

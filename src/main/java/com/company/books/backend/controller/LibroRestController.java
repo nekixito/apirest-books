9ad01b526e0936bd0ba5c7968309a34c4799f460
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2025-12-02 19:32:25 -0600

    Controller con API REST para obtener lista de libros.

---
+++ src/main/java/com/company/books/backend/controller/LibroRestController.java
@@ -0,0 +1,26 @@
+package com.company.books.backend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.company.books.backend.response.LibroResponseRest;
+import com.company.books.backend.service.ILibroService;
+
+@RestController
+@RequestMapping("/v1")
+public class LibroRestController {
+	
+	@Autowired
+	private ILibroService service;
+	
+	@GetMapping("/libros")
+	public ResponseEntity<LibroResponseRest> consultaLibros(){
+		
+		ResponseEntity<LibroResponseRest> response = service.buscarLibros();
+		return response;
+	}
+
+}

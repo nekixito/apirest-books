5cee1541e08d15ea44f346605111c0c89b78ea40
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2025-12-18 14:32:05 -0600

    Generando clases respuesta y request. Ignora clase servicio de token.

--- src/main/java/com/company/books/backend/request/AuthRequest.java
+++ src/main/java/com/company/books/backend/request/AuthRequest.java
@@ -1,5 +1,27 @@
 package com.company.books.backend.request;
 
-public class AuthRequest {
+import java.io.Serializable;
+
+public class AuthRequest implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private String usuario;
+	private String contrasenia;
+	
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getContrasenia() {
+		return contrasenia;
+	}
+	public void setContrasenia(String contrasenia) {
+		this.contrasenia = contrasenia;
+	}
+	
+	
 
 }
--- src/main/java/com/company/books/backend/response/TokenResponse.java
+++ src/main/java/com/company/books/backend/response/TokenResponse.java
@@ -1,5 +1,20 @@
 package com.company.books.backend.response;
 
 public class TokenResponse {
+	
+	private String jwtToken;
 
+	public TokenResponse(String jwtToken) {
+		this.jwtToken = jwtToken;
+	}
+
+	public String getJwtToken() {
+		return jwtToken;
+	}
+
+	public void setJwtToken(String jwtToken) {
+		this.jwtToken = jwtToken;
+	}
+	
+	
 }

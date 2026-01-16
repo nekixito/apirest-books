8e4493c5fc797bbb659b07411ec3754890a0aceb
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2026-01-16 15:30:09 -0600

    Ciclo de prueba uinitaria, @BeforeEach y @AfterEach.

--- src/test/java/com/company/books/backend/ejemplos/junit/CalculadoraTest.java
+++ src/test/java/com/company/books/backend/ejemplos/junit/CalculadoraTest.java
@@ -5,10 +5,14 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CalculadoraTest {
+	
+	Calculadora calc;
 	
 	@BeforeAll
 	public static void primero() {
@@ -18,11 +22,20 @@ public class CalculadoraTest {
 	public static void ultimo() {
 		System.out.println("ultimo");
 	}
+	
+	@BeforeEach
+	public void instanciaObjeto() {
+		calc = new Calculadora();
+		System.out.println("@BeforeEach");
+	}
+	
+	@AfterEach
+	public void despuesTest() {
+		System.out.println("@AfterEach");
+	}
 
 	@Test
 	public void calculadoraAssertEqualTest() {
-		Calculadora calc = new Calculadora();
-		
 		assertEquals(2, calc.sumar(1, 1));
 		assertEquals(3, calc.restar(4, 1));
 		assertEquals(56, calc.multiplicar(7, 8));
@@ -32,7 +45,6 @@ public class CalculadoraTest {
 	
 	@Test
 	public void calculadoraTrueFalse() {
-		Calculadora calc = new Calculadora();
 		
 		assertTrue(calc.sumar(1, 1) == 2);
 		assertTrue(calc.restar(4, 1) == 3);

cb08f8332527bba2f7752d421975b1d6f026123e
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2026-01-16 12:31:31 -0600

    Ciclo de prueba uinitaria, @BeforeAll y @AfterAll.

--- src/test/java/com/company/books/backend/ejemplos/junit/CalculadoraTest.java
+++ src/test/java/com/company/books/backend/ejemplos/junit/CalculadoraTest.java
@@ -4,9 +4,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class CalculadoraTest {
+	
+	@BeforeAll
+	public static void primero() {
+		System.out.println("primero");
+	}
+	@AfterAll
+	public static void ultimo() {
+		System.out.println("ultimo");
+	}
 
 	@Test
 	public void calculadoraAssertEqualTest() {
@@ -16,6 +27,7 @@ public class CalculadoraTest {
 		assertEquals(3, calc.restar(4, 1));
 		assertEquals(56, calc.multiplicar(7, 8));
 		assertEquals(9, calc.dividir(27,3));
+		System.out.println("calculadoraAssertEqualTest");
 	}
 	
 	@Test
@@ -26,5 +38,6 @@ public class CalculadoraTest {
 		assertTrue(calc.restar(4, 1) == 3);
 		assertFalse(calc.multiplicar(7, 8) == 9);
 		assertFalse(calc.dividir(4,2) == 1);
+		System.out.println("calculadoraTrueFalse");
 	}
 }
